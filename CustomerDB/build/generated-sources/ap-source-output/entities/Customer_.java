a92b7aa51346431b76d787725040f02c2e600c6f
jimmy90321 <jimmy4302001@gmail.com>
2017-09-22 23:11:02 +0800

    move web server to github repository

---
+++ CustomerDB/build/generated-sources/ap-source-output/entities/Customer_.java
@@ -0,0 +1,16 @@
+package entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-09-05T01:10:08")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, Integer> custid;
+    public static volatile SingularAttribute<Customer, String> userpw;
+    public static volatile SingularAttribute<Customer, String> email;
+    public static volatile SingularAttribute<Customer, String> username;
+
+}
---
+++ CustomerDB/src/java/entities/Customer.java
@@ -0,0 +1,120 @@
+
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name = "customer")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
+    , @NamedQuery(name = "Customer.findByCustid", query = "SELECT c FROM Customer c WHERE c.custid = :custid")
+    , @NamedQuery(name = "Customer.findByUsername", query = "SELECT c FROM Customer c WHERE c.username = :username")
+    , @NamedQuery(name = "Customer.findByUserpw", query = "SELECT c FROM Customer c WHERE c.userpw = :userpw")
+    , @NamedQuery(name = "Customer.findByEmail", query = "SELECT c FROM Customer c WHERE c.email = :email")})
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "custid")
+    private Integer custid;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "userpw")
+    private String userpw;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 50)
+    @Column(name = "email")
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(Integer custid) {
+        this.custid = custid;
+    }
+
+    public Customer(Integer custid, String username, String userpw) {
+        this.custid = custid;
+        this.username = username;
+        this.userpw = userpw;
+    }
+
+    public Integer getCustid() {
+        return custid;
+    }
+
+    public void setCustid(Integer custid) {
+        this.custid = custid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserpw() {
+        return userpw;
+    }
+
+    public void setUserpw(String userpw) {
+        this.userpw = userpw;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (custid != null ? custid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Customer)) {
+            return false;
+        }
+        Customer other = (Customer) object;
+        if ((this.custid == null && other.custid != null) || (this.custid != null && !this.custid.equals(other.custid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.Customer[ custid=" + custid + " ]";
+    }
+
+}
