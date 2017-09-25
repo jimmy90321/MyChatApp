32634b6956aec67da003ad3a7f8b9c24950f1011
jimmy90321 <jimmy4302001@gmail.com>
2017-09-25 19:22:51 +0800

    20170925
    新增點擊EditText外隱藏鍵盤

--- MyChatApp/app/src/main/java/com/example/jimmy/mychatapp/LoginActivity.java
+++ MyChatApp/app/src/main/java/com/example/jimmy/mychatapp/LoginActivity.java
@@ -4,38 +4,57 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ScrollView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private ScrollView sv_login;
-    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            View view = LoginActivity.this.getWindow().peekDecorView();
-            if(view != null && view.getWindowToken() != null){
-                try{
-                    ((InputMethodManager)LoginActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(LoginActivity.this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                }catch (Exception ex){
-                    Log.d("Auto-close keyboard","自動隱藏鍵盤異常");
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() == MotionEvent.ACTION_DOWN){
+            View v = getCurrentFocus();
+            if(isShouldHideInput(v,ev)){
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(imm != null){
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
                 }
             }
+            return super.dispatchTouchEvent(ev);
         }
-    };
+        if(getWindow().superDispatchTouchEvent(ev)){
+            return true ;
+        }
+        return onTouchEvent(ev);
+    }
+
+    private boolean isShouldHideInput(View v, MotionEvent ev) {
+        if(v != null && (v instanceof EditText)){
+            int[] leftTop = {0,0};
+
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if(ev.getX() > left && ev.getX() < right && ev.getY() > top && ev.getY() < bottom){
+                return false;
+            }else return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        sv_login = (ScrollView)findViewById(R.id.sv_login);
-        sv_login.setOnClickListener(mOnClickListener);
+
 
         TextView newRegister = (TextView)findViewById(R.id.tv_new_register);
+
         newRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- MyChatApp/app/src/main/java/com/example/jimmy/mychatapp/RegisterActivity.java
+++ MyChatApp/app/src/main/java/com/example/jimmy/mychatapp/RegisterActivity.java
@@ -1,11 +1,14 @@
 package com.example.jimmy.mychatapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +19,40 @@ public class RegisterActivity extends AppCompatActivity {
 
     private EditText et_email,et_pass,et_passcofirm,et_name;
     private TextView tv_readyToLogin,tv_emailhint,tv_passwordhint,tv_confirmpwhint;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() == MotionEvent.ACTION_DOWN){
+            View v = getCurrentFocus();
+            if(isShouldHideInput(v,ev)){
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                if(imm != null){
+                    imm.hideSoftInputFromWindow(v.getWindowToken(),0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        if(getWindow().superDispatchTouchEvent(ev)){
+            return true ;
+        }
+        return onTouchEvent(ev);
+    }
+
+    private boolean isShouldHideInput(View v, MotionEvent ev) {
+        if(v != null && (v instanceof EditText)){
+            int[] leftTop = {0,0};
+
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if(ev.getX() > left && ev.getX() < right && ev.getY() > top && ev.getY() < bottom){
+                return false;
+            }else return true;
+        }
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
