806bc3771c502b654a82e1b5458bd34a34db2137
bomy <toc00337@gmail.com>
2017-10-22 15:53:01 +0800

    [Add]  api protocal of errMessage

--- MyChatAppWebServer/src/java/entities/Users.java
+++ MyChatAppWebServer/src/java/entities/Users.java
@@ -136,13 +136,13 @@ public class Users implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Users)) {
-            return false;
-        }
-        Users other = (Users) object;
-        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
-            return false;
-        }
+         //if (!(object instanceof Users)) {
+           //  return false;
+        // }
+       //  Users other = (Users) object;
+       //  if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
+        //     return false;
+       //  }
         return true;
     }
 
